115108c100c4d1e761d3bbb7e8436c8ee0c54399
Chynara1 <94482672+Chynara1@users.noreply.github.com>
2022-04-13 21:01:56 -0400

    Added age and salary

--- src/test/java/steps/Chynara.java
+++ src/test/java/steps/Chynara.java
@@ -3,5 +3,7 @@ package steps;
 public class Chynara {
     public static void main(String[] args) {
         String name = "Chynara";
+        int age = 33;
+        double salary = 114.00;
     }
 }
